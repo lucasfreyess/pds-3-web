0d101af8b686dd19df86fb7db34f36b27f5c41a3
caroohong <cbhong@miuandes.cl>
2024-11-12 20:40:52 -0300

    mqtt

---
+++ app/controllers/mqtt_controller.rb
@@ -0,0 +1,41 @@
+# app/controllers/mqtt_controller.rb
+class MqttController < ApplicationController
+    def send_message
+      # Dirección IP del broker remoto o dominio
+      broker_ip = ""
+  
+      # Conexión al broker
+      client = MQTT::Client.new("mqtt://#{broker_ip}")
+  
+      # Conéctate al broker
+      client.connect
+  
+      # Publica un mensaje en un tópico específico
+      client.publish('esp32/topic', 'Mensaje desde Rails')
+  
+      # Desconéctate del broker
+      client.disconnect
+  
+      render json: { message: 'Mensaje enviado a MQTT' }
+    end
+  
+    def subscribe_to_topic
+      # Dirección IP del broker remoto o dominio
+      broker_ip = "" 
+  
+      # Conéctate al broker
+      client = MQTT::Client.new("mqtt://#{broker_ip}")
+      client.connect
+  
+      # Suscríbete al tópico y define el comportamiento cuando se reciba un mensaje
+      client.get('esp32/topic') do |topic, message|
+        Rails.logger.info "Mensaje recibido en el tópico #{topic}: #{message}"
+      end
+  
+      # Desconéctate después de recibir el mensaje
+      client.disconnect
+  
+      render json: { message: 'Escuchando los mensajes en el tópico' }
+    end
+  end
+  
